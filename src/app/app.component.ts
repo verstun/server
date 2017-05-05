cf7acf584516f3fc79e622fddfcace4030f4a758
verstun <svetazavolokina81@gmail.com>
2017-05-04 20:24:40 -0400

    lifecycles hooks in action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements =[{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+    serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
 
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
-     this.serverElements.push({
-     type:'server',
-     name: serverData.serverName,
-     content: serverData.serverContent
-     });
-  }
+    onServerAdded(serverData:{serverName: string, serverContent: string}) {
+        this.serverElements.push({
+            type: 'server',
+            name: serverData.serverName,
+            content: serverData.serverContent
+        });
+    }
 
-  onBluePrintAdded(blueprintData: {serverName: string, serverContent: string}) {
-     this.serverElements.push({
-     type: 'blueprint',
-     name: blueprintData.serverName,
-     content: blueprintData.serverContent
-     }
-     );
-  }
+    onBluePrintAdded(blueprintData:{serverName: string, serverContent: string}) {
+        this.serverElements.push({
+                type: 'blueprint',
+                name: blueprintData.serverName,
+                content: blueprintData.serverContent
+            }
+        );
+    }
+
+    onChangeFirst() {
+        this.serverElements[0].name = 'Changed';
+    }
+
+    onDestroyFirst(){
+        this.serverElements.splice(0,1);
+    }
 }
